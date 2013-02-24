dbed05a638fb0eb99a81d153f215dd769c4420c6
Walter S. Lasecki <wlasecki@roc.cs.rochester.edu>
2013-02-24 09:41:16 -0500

    initial db version and skeleton php code

--- issue_task.php
+++ issue_task.php
@@ -7,12 +7,12 @@ include('getDB.php');
 
 
 // Make sure a session is defined
-if( isset($_REQUEST['session']) && isset($_REQUEST['round'])  ) {
-  $session = $_REQUEST['session'];
-  $round = intval($_REQUEST['round']);
+if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_REQUEST['hitId'])&& isset($_REQUEST['requesterId'])  ) {
+  $worker = $_REQUEST['workerId'];
+  $assignment = $_REQUEST['assignmentId'];
+  $hit = $_REQUEST['hitId'];
+  $requester = $_REQUEST['requesterId'];
 
-
-  $id = session_id();
 
   // Try to connect to the DB
   try {
@@ -23,7 +23,7 @@ if( isset($_REQUEST['session']) && isset($_REQUEST['round'])  ) {
 
   // If the DB connection was made correctly...
   if($dbh) {
-    $sth = $dbh->prepare ("SELECT done, role, user, cUsers.id AS user_id, chat, time, round, c.id AS chat_id FROM cChats c, cSessions, cUsers WHERE cUsers.id = c.user_id AND cSessions.session = :session AND round <= :round AND cSessions.id = c.session_id AND c.id > :lastid ORDER BY time");
+    $sth = $dbh->prepare ("SELECT task FROM tasks WHERE xxxxxxx ORDER BY RAND()");
     $sth->execute(array(':session'=>$session, ':lastid'=>$lastid, ':round'=>$round));
 
     while( $row = $sth->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
