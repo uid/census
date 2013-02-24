59800e2a081431b1ee19efef79e74d5487814a1d
Walter S. Lasecki <wlasecki@roc.cs.rochester.edu>
2013-02-24 10:31:00 -0500

    initial db version and skeleton php code

--- issue_task.php
+++ issue_task.php
@@ -8,29 +8,29 @@ include('getDB.php');
 
 // Make sure a session is defined
 if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_REQUEST['hitId'])&& isset($_REQUEST['requesterId'])  ) {
-  $worker = $_REQUEST['workerId'];
-  $assignment = $_REQUEST['assignmentId'];
-  $hit = $_REQUEST['hitId'];
-  $requester = $_REQUEST['requesterId'];
+	$worker = $_REQUEST['workerId'];
+	$assignment = $_REQUEST['assignmentId'];
+	$hit = $_REQUEST['hitId'];
+	$requester = $_REQUEST['requesterId'];
 
 
-  // Try to connect to the DB
-  try {
-    $dbh = getDatabaseHandle();
-  } catch(PDOException $e) {
-    echo $e->getMessage();
-  }
+	// Try to connect to the DB
+	try {
+		$dbh = getDatabaseHandle();
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
 
-  // If the DB connection was made correctly...
-  if($dbh) {
-    $sth = $dbh->prepare ("SELECT task FROM tasks WHERE xxxxxxx ORDER BY RAND()");
-    $sth->execute(array(':session'=>$session, ':lastid'=>$lastid, ':round'=>$round));
+	// If the DB connection was made correctly...
+	if($dbh) {
+		$sth = $dbh->prepare ("SELECT tasks.id,task, workerid,hitid,taskid FROM tasks LEFT OUT JOIN requests ON tasks.id=taskid WHERE xxxx ORDER BY RAND()");
+		$sth->execute(array(':worker'=>$worker, ':hit'=>$hit));
 
-    while( $row = $sth->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
-	//
-    }
+		while( $row = $sth->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT) ) {
+			//
+		}
 
-    print "}";
-  }
+		print "}";
+	}
 }
 ?>
