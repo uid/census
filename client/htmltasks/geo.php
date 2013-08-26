2582c86f020759c8a468b6515fa3130961281bea
Maitreyee Joshi <maitreyeejoshi@dn0a2315bf.sunet>
2013-08-26 15:42:33 -0700

    geo.php

--- client/htmltasks/geo.php
+++ client/htmltasks/geo.php
@@ -16,6 +16,8 @@
 
 //$ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 $ip = $_SERVER['REMOTE_ADDR'];
+$ip = '171.66.174.65';
+
 echo "Either your ip <b>or</b> the ip of the closest proxy server to me is:<br><br>
  <font size='+1'>" . $ip . "</font><br><br>
  <img src='http://auroramag.files.wordpress.com/2011/12/thumb-up-for-2012-copy.jpg?w=150&h=138'>
@@ -26,7 +28,18 @@ echo "Either your ip <b>or</b> the ip of the closest proxy server to me is:<br><
 
 echo "Now I am going to try to figure out where that is...<br><br><font size='+1'>";
 
-print_r(geoCheckIP($ip));
+//print_r(geoCheckIP($ip));
+$data = geoCheckIP($ip);
+echo "<br />"."IP Address: ";
+echo $data["ip"];
+echo "<br />"."Country: ";
+echo $data["country_name"];
+echo "<br />"."Region Name: ";
+echo $data["region_name"];
+echo "<br />"."City: ";
+echo $data["city"];
+echo "<br />"."Zip Code: ";
+echo $data["zipcode"];
 
 function geoCheckIP($ip)
 {
@@ -38,13 +51,14 @@ function geoCheckIP($ip)
 
 	//contact ip-server
 	$response=@file_get_contents('http://www.freegeoip.net/json/'.$ip);
+	$locations=json_decode($response, true);
 
 	if (empty($response))
 	{
 	  throw new InvalidArgumentException("Error contacting Geo-IP-Server");
 	}
 
-	return $response;
+	  return $locations;
 }
 ?>
 
--- issue_task.php
+++ issue_task.php
@@ -5,6 +5,29 @@ ini_set("display_errors", 1);
 
 // Use a shared connection file
 include('getDB.php');
+include('geo.php');
+
+//$ip = $_SERVER['REMOTE_ADDR'];
+//$ip = 171.66.185.85;
+//$response=@file_get_contents('http://www.freegeoip.net/json/'.$ip);
+//$locations=json_decode($response, true);
+//$geoinfo = $locations;
+//$geoinfo = geoCheckIP($ip);
+//$country = $geoinfo['country_name'];
+$country = "United States";
+
+$_REQUEST['workerId'] = "testWorker";
+$_REQUEST['assignmentId'] = 9999;
+$_REQUEST['hitId'] = 9999;
+$_REQUEST['requesterId'] = 9999;
+
+
+$output =   "workerId: " . (isset($_REQUEST['workerId']) ? $_REQUEST['workerId'] : " not set :(") . "\n" . 
+			"assignmentId: " . (isset($_REQUEST['assignmentId']) ? $_REQUEST['assignmentId'] : " not set :(") . "\n" .
+			"hitId: " . (isset($_REQUEST['hitId']) ? $_REQUEST['hitId'] : " not set :(") . "\n" .
+			"requesterId: " . (isset($_REQUEST['requesterId']) ? $_REQUEST['requesterId'] : " not set :(") . "\n" ;
+
+file_put_contents('request.txt', $output);
 
 // Make sure a session is defined
 if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_REQUEST['hitId']) && isset($_REQUEST['requesterId'])  ) {
@@ -20,11 +43,15 @@ if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_
 		echo $e->getMessage();
 	}
 
+
 	// If the DB connection was made correctly...
 	if($dbh) {
 		$sth = $dbh->prepare ("SELECT tasks.id,summary,content FROM tasks LEFT OUTER JOIN requests ON tasks.id=taskid AND workerid=:worker AND hitid=:hit ORDER BY RAND() LIMIT 1");
 		$sth->execute(array(':worker'=>$worker, ':hit'=>$hit));
 			$row = $sth->fetch(PDO::FETCH_ASSOC);
+
+		file_put_contents('log.txt', 'hello');
+
 		if( $row['id'] == null ) {
 			$data = array(
 		  		"success"=>false,
@@ -33,9 +60,19 @@ if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_
 		  		"request_id"=>-1
 			);
 		} else {
-			$sth = $dbh->prepare('INSERT INTO requests (requesterid, workerid, hitid, ip, mac, data, browser, taskid) VALUES (:requester, :worker, :hit, :ip, "", :data, "", :assignment)');
-			$sth->execute(array(':requester'=>$requester, ':worker'=>$worker, ':hit'=>$hit, ':ip'=>$_SERVER['REMOTE_ADDR'], 
-				':data'=>serialize($_SERVER), ':assignment'=>$row['id']));
+
+			$query = ('INSERT INTO requests (requesterid, workerid, hitid, ip, mac, data, browser, taskid, country) 
+				VALUES (:requester, :worker, :hit, :ip, "", :data, "", :assignment, "' . $country . '")');
+
+			$sth = $dbh->prepare($query);
+
+			file_put_contents("debug.txt", $query);
+
+			//$sth = $dbh->prepare('INSERT INTO requests (requesterid, workerid, hitid, ip, mac, data, browser, taskid, country) 
+			//	VALUES (:requester, :worker, :hit, :ip, "", :data, "", :assignment, $country)');
+
+			$sth->execute(array(':requester'=>$requester, ':worker'=>$worker, ':hit'=>$hit, ':ip'=>$_SERVER['REMOTE_ADDR'],  
+				':data'=>serialize($_SERVER), ':assignment'=>$row['id'], ':country'=>$country));
 
 			$data = array(
 		  		"success"=>true,
@@ -44,7 +81,7 @@ if( isset($_REQUEST['workerId']) && isset($_REQUEST['assignmentId']) && isset($_
 		  		"request_id"=>$dbh->lastInsertId()
 			);
 		}
-		echo $_GET['callback'] . '('.json_encode($data).')';		
+		echo $_GET['callback'] . '('.json_encode($data).')';
 	}
 }
 ?>
