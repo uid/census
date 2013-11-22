a7ace6a58b28a0d59e52cb8679a9f8271c9fcc64
wslasecki <wslasecki@gmail.com>
2013-11-22 18:40:21 +0000

    trying a live HIT

--- util/mturk/createExternalHIT.py
+++ util/mturk/createExternalHIT.py
@@ -2,14 +2,13 @@ from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import ExternalQuestion
 from awskeys import AWS_ACCESS_KEY, AWS_SECRET_KEY
 
-
 SANDBOX = True
 
 NUMBER_OF_HITS = 1
-NUMBER_OF_ASSIGNMENTS = 3
-LIFETIME = 60 * 24 * 0.1
-TITLE = 'Identify emotions provoked'
-EXPLANATION = 'Score the news headline for how much it provokes specific emotions'
+NUMBER_OF_ASSIGNMENTS = 1
+LIFETIME = 60 * 60 * 24
+TITLE = 'Five question survey about the web and your community'
+EXPLANATION = 'We are investigating how much the web and your community interact. This is a five-question survey, all agree/disagree questions.'
 
 def create_hits():
 	if SANDBOX:
@@ -19,12 +18,12 @@ def create_hits():
 		mturk_url = 'mechanicalturk.amazonaws.com'
 		preview_url = 'https://mturk.com/mturk/preview?groupId='
 
-	q = ExternalQuestion(external_url="https://census.stanford.edu/client/maintask.html", frame_height=800)
+	q = ExternalQuestion(external_url="https://census.stanford.edu/client/demo/maintask.html", frame_height=800)
 	conn = MTurkConnection(aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY, host=mturk_url)
 	keywords=['census']
 	for i in range(0, NUMBER_OF_HITS):
 		create_hit_rs = conn.create_hit(question=q, lifetime=LIFETIME,max_assignments=NUMBER_OF_ASSIGNMENTS,title=TITLE, keywords=keywords,reward = 0.05, duration=60*6,approval_delay=60*60, annotation=EXPLANATION)
-		print(preview_url + create_hit_rs[0].HITId)
+		print(preview_url + create_hit_rs[0].HITTypeId)
     
 if __name__ == "__main__":
 	create_hits()
