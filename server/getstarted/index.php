618b43996347fbb860186198af4a85e3ba551874
wslasecki <wslasecki@gmail.com>
2013-12-12 10:57:49 +0000

    working keygen

--- server/getstarted/index.php
+++ server/getstarted/index.php
@@ -12,11 +12,29 @@
 
 	<div id="Instructions">
 		To get started, just click the button below and generate a unique key, then get a copy of the <a href="">Census GitHub project</a>
+
+		<BR/>
+		<BR/>
+		<HR/>
+		<BR/>
 	</div>
 
 	<div id="container">
+		<div id="pass" class="pwd">
+			<div id="pass-label">Add a password (optional):</div>
+			<input type="text" id="pass-field"></input>
+		</div>
+		<div id="confirmPass" class="pwd">
+			<div id="confirmPass-label">Confirm password:</div>
+			<input type="text" id="confirmPass-field"></input>
+		</div>
+
 		<input type="button" id="genBtn" value="Click here to get a key for your project!"></input>
-		<div id="key-val"></div>
+
+		<div id="key">
+			<div id="key-label">Your key: </div>
+			<div id="key-val"></div>
+		</div>
 	</div>
 
 </body>
